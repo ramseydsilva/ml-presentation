eb2425d6bdd01f27d685c1dd50be6facbc45a8d4
Bowei Yao <byao@mackenzieinvestments.com>
2017-09-20 10:23:36 -0400

    Create a neural network class and use it for training and prediction

---
+++ neural_network.py
@@ -0,0 +1,50 @@
+from numpy import exp, array, random, dot
+
+
+class NeuralNetwork():
+    def __init__(self, input_count, output_count, seed):
+        # Seed the random number generator, so it generates the same numbers
+        # every time the program runs.
+        random.seed(seed)
+
+        # We model a single neuron, with 3 input connections and 1 output connection.
+        # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
+        # and mean 0.
+        self.synaptic_weights = 2 * random.random((input_count, output_count)) - 1
+
+    # The Sigmoid function, which describes an S shaped curve.
+    # We pass the weighted sum of the inputs through this function to
+    # normalise them between 0 and 1.
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+
+    # The derivative of the Sigmoid function.
+    # This is the gradient of the Sigmoid curve.
+    # It indicates how confident we are about the existing weight.
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    # We train the neural network through a process of trial and error.
+    # Adjusting the synaptic weights each time.
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
+            # Pass the training set through our neural network (a single neuron).
+            output = self.predict(training_set_inputs)
+
+            # Calculate the error (The difference between the desired output
+            # and the predicted output).
+            error = training_set_outputs - output
+
+            # Multiply the error by the input and again by the gradient of the Sigmoid curve.
+            # This means less confident weights are adjusted more.
+            # This means inputs, which are zero, do not cause changes to the weights.
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            # Adjust the weights.
+            self.synaptic_weights += adjustment
+
+    # The neural network thinks.
+    def predict(self, inputs):
+        # Pass inputs through our neural network (our single neuron).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
